403304c19287e910c04230be9fa6bbd4aed80345
LaikaFusion <newrad4@gmail.com>
2018-07-20 13:01:42 -0400

    Finished plate constructor

--- js/index.js
+++ js/index.js
@@ -9,19 +9,28 @@ class PlateCard{
         this.abbreviation = element.abbreviation;
         this.img = element.img;
         this.pointValue = element.pointValue;
-        this.seen = false;
+        this.found = false;
+        console.log(this.element);
     }
+        
     seenToggle(){
+        debugger;
         if(this.seen){
             this.seen = false;
+            if(document.getElementById(`${this.abbreviation}`).classList.contains('found')){
+                document.getElementById(`${this.abbreviation}`).classList.remove('found');
+            }
+            
         }
         else{
             this.seen = true;
+            document.getElementById(`${this.abbreviation}`).classList.add('found');
         }
     }
     createCard(){
         let divConstr = document.createElement("div");
         divConstr.className = "license-plate";
+        divConstr.setAttribute('id', `${this.abbreviation}`);
         let titleSpan = document.createElement("span");
         titleSpan.className = "licenesePlateTitle";
         titleSpan.textContent = `${this.name}`
@@ -30,8 +39,16 @@ class PlateCard{
         plateImg.className = "plateImage";
         plateImg.setAttribute('src', `${this.img}`);
         divConstr.appendChild(plateImg);
+        divConstr.addEventListener('click', () => {
+            this.seenToggle();
+          });   
         licenesePlateArea.appendChild(divConstr);
+        
     }
 
 }
 
+plates.forEach(element => {
+    const plate = new PlateCard(element);
+    plate.createCard();
+});
